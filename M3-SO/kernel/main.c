0e3736c8b595625a10b0dc9bb4a5aa40583f7093
Carlos Miguel Bernardo <159397195+carlosmbernardo@users.noreply.github.com>
2026-07-06 23:51:33 +0000

    Feat: Junção kernel e módulo trabalho

---
+++ M3-SO/kernel/main.c
@@ -0,0 +1,48 @@
+#include "task.h"
+#include "scheduler.h"
+#include "memory.h"
+#include "uart.h"
+#include "treefs.h"
+extern void uart_print(const char*);
+/*   Tasks   */
+void task1()
+{
+    while (1)
+    {
+        uart_print("Task 1 running\n");
+        uart_print("Memory used: ");
+        uart_print_uint(memory_used());
+        uart_print(" bytes\n");
+        uart_print("Memory free: ");
+        uart_print_uint(memory_free());
+        uart_print(" bytes\n\n");
+        yield();
+    }
+}
+void task2()
+{
+    while (1)
+    {
+        uart_print("Task 2 running\n");
+        uart_print("Memory used: ");
+        uart_print_uint(memory_used());
+        uart_print(" bytes\n");
+        uart_print("Memory free: ");
+        uart_print_uint(memory_free());
+        uart_print(" bytes\n\n");
+        yield();
+    }
+}
+/*   Kernel   */
+void kernel_main()
+{
+    memory_init();   // OBRIGATÓRIO
+    uart_print("\n=== Kernel ===\n");
+
+    treefs_run_demo();   // demonstracao do TreeFS (8 cenarios) antes do scheduler
+
+    xTaskCreate(task1, 2048, 1);
+    xTaskCreate(task2, 2048, 1);
+    scheduler_start();
+    while (1);
+}
---
+++ M3-SO/kernel/task.c
@@ -0,0 +1,33 @@
+#include "task.h"
+#include "memory.h"
+#include <stdint.h>
+
+TCB tasks[MAX_TASKS];
+int task_count = 0;
+
+void xTaskCreate(void (*task)(void),
+                 uint32_t stack_size,
+                 int priority)
+{
+    if (task_count >= MAX_TASKS)
+        return;
+
+    if (stack_size == 0)
+        stack_size = DEFAULT_STACK_SIZE;
+
+    TCB *t = &tasks[task_count];
+
+    t->stack = (uint8_t*)kmalloc(stack_size);
+
+    uint64_t *sp = (uint64_t*)(t->stack + stack_size);
+
+    /* Configurar contexto inicial */
+
+    t->regs[0] = (uint64_t)task;   // ra
+    t->regs[1] = (uint64_t)sp;     // sp
+
+    t->entry = task;
+    t->priority = priority;
+
+    task_count++;
+}
